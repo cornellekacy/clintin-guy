d26c74ea59558d9890e71f0b240bc6341344d53d
cornellekacy <cornellekacy4@gmail.com>
2020-03-07 19:21:56 +0400

    2

--- backend/conn.php
+++ backend/conn.php
@@ -1,11 +1,2 @@
-<?php $link = mysqli_connect("localhost", "gqtyvvybht", "Xcuh2E5yN4", "gqtyvvybht"); ?>
+<?php $link = mysqli_connect("localhost", "root", "", "track"); ?>
 
-<?php
-// $mysqli = new mysqli("localhost","azuerbxxqq","W239ejZRrp","azuerbxxqq");
-
-// // Check connection
-// if ($mysqli -> connect_errno) {
-//   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-//   exit();
-// }
-// ?>
